7ba0b42e3dc65cadde25a8043be129cbaefe8fbc
shauritanga <curtisisaac36@gmail.com>
2026-03-16 02:00:19 +0300

    added seeker profile

--- src/modules/seeker-profile/seeker-profile.module.ts
+++ src/modules/seeker-profile/seeker-profile.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SeekerProfileService } from './seeker-profile.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { SeekerProfileController } from './seeker-profile.controller';
 
 @Module({
   imports: [PrismaModule],
+  controllers: [SeekerProfileController],
   providers: [SeekerProfileService],
   exports: [SeekerProfileService],
 })
